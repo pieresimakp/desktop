c03f22b56124c9bd8ccc73f03cd92f259565479a
Pier Simak <69743585+pieresimakp@users.noreply.github.com>
2025-08-26 16:58:28 +0700

    ✨ [FEATURE] done tagging

--- app/src/ui/changes/commit-message.tsx
+++ app/src/ui/changes/commit-message.tsx
@@ -798,7 +798,10 @@ export class CommitMessage extends React.Component<
 
   private renderEmojiButton() {
     return (
-      <CommitMessageEmoji/>
+      <CommitMessageEmoji
+        commitValue={this.state.commitMessage.summary}
+        commitValueChanged={this.onSummaryChanged}
+      />
     )
   }
 
