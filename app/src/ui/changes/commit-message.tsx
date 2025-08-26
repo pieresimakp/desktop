f9d4f91732458ad035e63262fe543e754decf06e
Pier Simak <69743585+pieresimakp@users.noreply.github.com>
2025-08-26 16:27:40 +0700

    added emoji

--- app/src/ui/changes/commit-message.tsx
+++ app/src/ui/changes/commit-message.tsx
@@ -69,6 +69,7 @@ import {
 import { AriaLiveContainer } from '../accessibility/aria-live-container'
 import { HookProgress } from '../../lib/git'
 import { assertNever } from '../../lib/fatal-error'
+import { CommitMessageEmoji } from './commit-message-emoji'
 
 const addAuthorIcon: OcticonSymbolVariant = {
   w: 18,
@@ -792,6 +793,12 @@ export class CommitMessage extends React.Component<
         repository={repository}
         accounts={this.props.accounts}
       />
+    )
+  }
+
+  private renderEmojiButton() {
+    return (
+      <CommitMessageEmoji/>
     )
   }
 
@@ -1743,7 +1750,7 @@ export class CommitMessage extends React.Component<
       >
         <div className={summaryClassName} ref={this.summaryGroupRef}>
           {this.renderAvatar()}
-
+          {this.renderEmojiButton()}
           <AutocompletingInput
             required={true}
             label={this.props.showInputLabels === true ? 'Summary' : undefined}
